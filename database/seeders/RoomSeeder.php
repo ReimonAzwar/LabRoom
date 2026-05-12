9328be7f653a6aa4ccb9e570c797122cea723e15
ReimonAzwar <h1051231034@student.untan.ac.id>
2026-05-12 09:47:48 +0700

    Add files via upload

--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -14,12 +14,9 @@ class RoomSeeder extends Seeder
     public function run(): void
     {
         $rooms = [
-            ['nama_ruang' => 'Lab Komputer A', 'kapasitas' => 30, 'fasilitas' => '30 PC, Proyektor, AC, WiFi, Whiteboard', 'status' => 'dibuka'],
-            ['nama_ruang' => 'Lab Komputer B', 'kapasitas' => 30, 'fasilitas' => '30 PC, Proyektor, AC, WiFi, Printer', 'status' => 'dibuka'],
-            ['nama_ruang' => 'Lab Jaringan', 'kapasitas' => 24, 'fasilitas' => '24 PC, Rack Server, Switch, Router, Kabel UTP', 'status' => 'dibuka'],
-            ['nama_ruang' => 'Lab Elektronika', 'kapasitas' => 20, 'fasilitas' => 'Osiloskop, PCB, Solder, Power Supply, Multimeter', 'status' => 'dibuka'],
-            ['nama_ruang' => 'Ruang Seminar', 'kapasitas' => 60, 'fasilitas' => 'Meja Bundar, Proyektor, Sound System, AC, Papan Tulis', 'status' => 'dibuka'],
-            ['nama_ruang' => 'Ruang Rapat', 'kapasitas' => 15, 'fasilitas' => 'Meja Rapat, Proyektor, TV, AC, Whiteboard', 'status' => 'dibuka'],
+            ['name' => 'Lab Terpadu 1', 'cap' => 40, 'fasilitas' => '40 PC, Proyektor, AC, WiFi, Whiteboard', 'status' => 'available'],
+            ['name' => 'Lab Terpadu 2', 'cap' => 40, 'fasilitas' => '40 PC, Proyektor, AC, WiFi, Whiteboard', 'status' => 'available'],
+            ['name' => 'Lab Terpadu 3', 'cap' => 40, 'fasilitas' => '40 PC, Proyektor, AC, WiFi, Whiteboard', 'status' => 'available'],
         ];
 
         foreach ($rooms as $room) {
